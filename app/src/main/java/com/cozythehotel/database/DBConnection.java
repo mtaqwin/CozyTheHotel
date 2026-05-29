6be28300ce18e623f474cc3826ea4b115eb03f72
mtaqwin <muhtakwiny@gmail.com>
2026-05-29 16:09:19 +0800

    build(build.gradle): menambahkan konfigurasi dependency javaFX

--- app/src/main/java/com/cozythehotel/database/DBConnection.java
+++ app/src/main/java/com/cozythehotel/database/DBConnection.java
@@ -3,6 +3,7 @@ package com.cozythehotel.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnection {
     private static final String URL = "jdbc:sqlite:cozy_hotel.db";
